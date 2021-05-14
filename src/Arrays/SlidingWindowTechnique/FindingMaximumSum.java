6175fa73cefa531c80dd49458b4cf73dec889bf4
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-05-14 23:52:57 +0530

    Findiing SubArray with given sum

--- src/Arrays/SlidingWindowTechnique/FindingMaximumSum.java
+++ src/Arrays/SlidingWindowTechnique/FindingMaximumSum.java
@@ -1,5 +1,68 @@
+
+/*
+ * 
+ * JAVA Code for Find maximum (or minimum) 
+ * sum of a subarray of size k 
+ * I/P:
+ * 		int arr[] = {1, 4, 2, 10, 2, 3, 1, 0, 20}; 
+		int k = 4;
+ *	O/P: 24
+ * 3+1+0+20 =24 is the maximum sum in this array with window size 4
+ *
+ * I/P:
+ * 		int arr[] = {1, 8, 30, -5, 20, 7}; 
+		int k = 3;
+ *	O/P: 45
+ * 30+(-5)+20 = 45 is the maximum sum in this array with window size 3
+ */
+
+
 package Arrays.SlidingWindowTechnique;
 
+import java.util.Scanner;
+
 public class FindingMaximumSum {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of the array");
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		
+		System.out.println("Enter window size");
+		int k = sc.nextInt();
+		
+		System.out.println("Enter Array elements");
+		for(int i=0;i<n;i++)
+			arr[i]=sc.nextInt();
+
+		System.out.println(maxSum(arr, n, k));
+		sc.close();
+	}
+
+	// Returns maximum sum in a subarray of size k. 
+	private static int maxSum(int[] arr, int n, int k) {
+
+		// k must be smaller than the size of array 
+		if(n<k) {
+			System.out.println("Invalid "+"Window size can't be greater than array Size");
+			return -1;
+		}
+		int currSum = 0;
+		for (int i = 0; i < k; i++)
+			currSum += arr[i];
+
+		int maxSum = currSum;
+
+		for (int i = k; i < n; i++) {
+			currSum += arr[i] - arr[i - k];
+			if (currSum > maxSum)
+				maxSum = currSum;
+		}
+
+		return maxSum;
+
+	}
 
 }
---
+++ src/Arrays/SlidingWindowTechnique/FindingSubArray.java
@@ -0,0 +1,43 @@
+/*
+ *  I/P:
+ *  	arr[] = {1, 4, 20, 3, 10, 5}
+ *  Sum: 33
+ *  O/P:
+ *  	Yes
+ *  
+ *  Explanation: Elements {20 + 3 + 10 = 33}
+ */
+
+package Arrays.SlidingWindowTechnique;
+
+public class FindingSubArray {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 4, 20, 3, 10, 5 };
+		int sum = 33;
+		System.out.println(isSubArrayThere(arr, arr.length, sum));
+	}
+
+	private static boolean isSubArrayThere(int arr[], int len, int sum) {
+
+		int curr_sum = arr[0];
+		int start = 0;
+
+		for (int i = 1; i < len; i++) {
+
+			// clear the previous window
+			while (curr_sum > sum && start < len - 1) {
+				curr_sum -= arr[start];
+				start++;
+			}
+			if(curr_sum == sum)
+				return true;
+			if(i<len)
+				curr_sum += arr[i];
+		}
+
+		return (curr_sum == sum);
+	}
+
+}
