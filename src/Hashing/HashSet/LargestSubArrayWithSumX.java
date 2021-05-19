d8273d6482e1677af8707e519f3874c63da1194f
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-05-19 23:46:39 +0530

    Made changes in previous 2 committed code

--- src/Hashing/HashSet/LargestSubArrayWithSumX.java
+++ src/Hashing/HashSet/LargestSubArrayWithSumX.java
@@ -1,10 +1,33 @@
 package Hashing.HashSet;
 
+import java.util.HashSet;
+
 public class LargestSubArrayWithSumX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int arr[] = new int[] { 8, 3, -7, -4, 1 };
+		int n = arr.length;
+		int sum = -8;
+
+		System.out.println(largestSubarrayWithSumX(arr, n, sum));
+	}
+
+	private static int largestSubarrayWithSumX(int[] arr, int n, int sum) {
+
+		int prefix_Sum = 0;
+		HashSet<Integer> hs = new HashSet<>();
+		hs.add(0);
+		for (int i = 0; i < n; i++) {
+			prefix_Sum += arr[i];
+
+			if (hs.contains(prefix_Sum - sum))
+				return 1;
+
+			hs.add(prefix_Sum);
+		}
+
+		return 0;
 	}
 
 }
--- src/Hashing/HashSet/LargestSubArrayWithSumZero.java
+++ src/Hashing/HashSet/LargestSubArrayWithSumZero.java
@@ -6,27 +6,27 @@ import java.util.Set;
 public class LargestSubArrayWithSumZero {
 
 	public static void main(String[] args) {
-		
-		int arr[] = new int[]{5, 3, 9, -4, -6, 7, -1};
-        int n = arr.length;
-        
-        System.out.println(ZeroSumSubarray(arr, n));
+
+		int arr[] = new int[] { 5, 3, 9, -4, -6, 7, -1 };
+		int n = arr.length;
+
+		System.out.println(ZeroSumSubarray(arr, n));
 	}
 
 	private static int ZeroSumSubarray(int[] arr, int n) {
-		Set<Integer> us = new HashSet<Integer>();
-        int prefix_sum = 0;
-        us.add(0);
-        for(int i = 0; i < n; i++)
-        {
-            prefix_sum += arr[i];
-            if(us.contains(prefix_sum) == true)
-              return 1;
-              
-            us.add(prefix_sum);
-        }
-        
-        return 0;
+		Set<Integer> s = new HashSet<Integer>();
+		int prefix_sum = 0;
+
+		s.add(0);
+
+		for (int i = 0; i < n; i++) {
+			prefix_sum += arr[i];
+			if (s.contains(prefix_sum) == true)
+				return 1;
+			s.add(prefix_sum);
+		}
+
+		return 0;
 
 	}
 
