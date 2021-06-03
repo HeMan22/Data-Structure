045e17f57c892e8d43f1c89777103ccbf0e36b26
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-06-03 23:56:42 +0530

    writing a program to determine winner of an election based on votes as well as following lexicographically order incase of same vote

---
+++ src/Hashing/HashMap/WinnerOfElection.java
@@ -0,0 +1,30 @@
+/*
+Example 1:
+
+Input:
+n = 13
+Votes[] = {john,johnny,jackie,johnny,john 
+jackie,jamie,jamie,john,johnny,jamie,
+johnny,john}
+Output: john 4
+Explanation: john has 4 votes casted for 
+him, but so does johny. john is 
+lexicographically smaller, so we print 
+john and the votes he received.
+Example 2:
+
+Input:
+n = 3
+Votes[] = {andy,blake,clark}
+Output: andy 1
+Explanation: All the candidates get 1 
+votes each. We print andy as it is 
+lexicographically smaller.
+*/
+
+
+package Hashing.HashMap;
+
+public class WinnerOfElection {
+
+}
