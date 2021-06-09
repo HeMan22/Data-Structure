82e50352515547193ef660993d6db8daa8ffbd1c
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-06-09 23:52:38 +0530

    added problem statement with examples

--- src/Hashing/HashMap/SubArrayRangeWithGivenSum.java
+++ src/Hashing/HashMap/SubArrayRangeWithGivenSum.java
@@ -1,3 +1,31 @@
+/*
+Given an unsorted array of integers and a sum.
+The task is to count the number of subArray which adds to the given sum.
+
+
+Example 1:
+
+Input:
+n = 5
+arr[] = {10,2,-2,-20,10}
+sum = -10
+Output: 3
+Explanation: Subarrays with sum -10 are: 
+[10, 2, -2, -20], [2, -2, -20, 10] and 
+[-20, 10].
+
+Example 2:
+
+Input:
+n = 6
+arr[] = {1,4,20,3,10,5}
+sum = 33
+Output: 1
+Explanation: Subarray with sum 33 is: 
+[20,3,10].
+*/
+
+
 package Hashing.HashMap;
 
 import java.util.HashMap;
