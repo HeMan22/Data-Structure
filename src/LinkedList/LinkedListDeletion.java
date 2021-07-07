5ed5cb9aa93ab5be90f6a09aebba50066031426a
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-07-07 23:49:35 +0530

    Linked List Deletion of the First occurence of the key

---
+++ src/LinkedList/LinkedListDeletion.java
@@ -0,0 +1,124 @@
+package LinkedList;
+
+public class LinkedListDeletion {
+
+	Node head;
+
+	private class Node {
+		Node next;
+		int data;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		LinkedListDeletion lld = new LinkedListDeletion();
+
+		lld.insertAtBegin(5);
+		lld.insertAtEnd(10);
+		lld.insertAtBegin(12);
+		lld.insertAtEnd(8);
+		lld.insertAtBegin(50);
+		lld.insertAtEnd(12);
+		lld.print();
+
+		lld.removeData(12); // Deleting FIrst Occurrence of the data
+		lld.print();
+
+		lld.removeNode(2); // Deleting Node at the given position
+		lld.print();
+	}
+
+	private void insertAtBegin(int x) {
+
+		Node temp = new Node(x);
+
+		temp.next = head;
+		head = temp;
+	}
+
+	private void insertAtEnd(int x) {
+
+		Node temp = new Node(x);
+
+		if (head == null) {
+			head = temp;
+		}
+		Node current = head;
+
+		while (current.next != null)
+			current = current.next;
+
+		// Now the Current Node reached End
+		current.next = temp;
+
+	}
+
+	private void removeData(int key) {
+
+		Node current = head;
+		Node prev = null;
+
+		// If Head Node itself holds the value to be deleted
+		if (current != null && current.data == key) {
+			// Change the head
+			head = current.next;
+			return;
+		}
+
+		while (current != null && current.data != key) {
+			prev = current;
+			current = current.next;
+		}
+
+		// If key is not present
+		if (current == null) {
+			System.out.println(key + ": Key is not present");
+			return;
+		}
+
+		// Unlink the node from Linked List
+		prev.next = current.next;
+
+	}
+
+	/*
+	 * Given a reference (pointer to pointer) to the
+	 * head of the list and a position, deletes the node at 
+	 * the given position
+	 */
+	private void removeNode(int position) {
+		
+		Node current =head;
+		
+		// If Link List is Empty
+		if(head==null) {
+			System.out.println("Nothing to Remove");
+			return;
+		}
+		
+		// If the head Node is to be removed
+		if(position==0) {
+			head=current.next;
+			return;
+		}
+		
+		// Finding the previous Node of the Node to be deleted
+		
+	}
+
+	private void print() {
+
+		Node current = head;
+
+		while (current != null) {
+			System.out.print(current.data + " ");
+
+			current = current.next;
+		}
+		System.out.println();
+	}
+}
