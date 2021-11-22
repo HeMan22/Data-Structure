8d4a31e068353d80dbab0aeffcd312d06bdef293
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-11-22 23:59:43 +0530

    Binary Search

---
+++ src/Searching/BinarySearch.java
@@ -0,0 +1,21 @@
+package Searching;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+	
+	public static void main(String...args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//Array Length
+		int len=sc.nextInt();
+		int arr[]=new int[len];
+		
+		for(int i=0;i<len;i++)
+			arr[i]=sc.nextInt();
+		
+		//Number to be Searched
+		int x= sc.nextInt();
+	}
+
+}
