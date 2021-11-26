373763f574e32abe7699f1480bf944dd286328d6
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-11-27 01:33:13 +0530

    BST Insertion

--- src/binarySearchTree/BSTInsertion.java
+++ src/binarySearchTree/BSTInsertion.java
@@ -2,22 +2,83 @@ package binarySearchTree;
 
 public class BSTInsertion {
 
-	/* Class containing left and right
-    child of current node and key value */
-	class Node{
+	/*
+	 * Class containing left and right child of current node and key value
+	 */
+	class Node {
 		int key;
-		Node left,right;
-		
+		Node left, right;
+
 		public Node(int item) {
-			key=item;
-			left=right=null;
+			key = item;
+			left = right = null;
 		}
 	}
-	
+
 	// Root of BST
-			Node root;
-	
-	BSTInsertion(){
-		root=null;
+	Node root;
+
+	BSTInsertion() {
+		root = null;
 	}
+
+	// This method mainly calls insertRec()
+	void insert(int key) {
+		root = insertRec(root, key);
+	}
+
+	Node insertRec(Node root, int key) {
+
+		/* If the tree is empty, return a new node */
+		if (root == null) {
+			root = new Node(key);
+			return root;
+		}
+
+		/* Otherwise, recur down the tree */
+		if (key < root.key)
+			root.left = insertRec(root.left, key);
+
+		else if (key > root.key)
+			root.right = insertRec(root.right, key);
+
+		/* return the (unchanged) node pointer */
+		return root;
+	}
+
+	// This method mainly calls inorderRec()
+	void inorder() {
+		inorderRec(root);
+	}
+
+	// A utility function to do inorder traversal of BST
+	void inorderRec(Node root) {
+
+		if (root != null) {
+			inorderRec(root.left);
+
+			System.out.print(root.key + " ");
+
+			inorderRec(root.right);
+		}
+
+	}
+
+	public static void main(String... args) {
+
+		BSTInsertion bst = new BSTInsertion();
+
+		bst.insert(50);
+		bst.insert(30);
+		bst.insert(20);
+		bst.insert(40);
+		bst.insert(70);
+		bst.insert(60);
+		bst.insert(80);
+
+		// print inorder traversal of the BST
+		bst.inorder();
+
+	}
+
 }
